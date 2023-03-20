0e3e764d7fcccbcbc953e82a8b3dacfc2cfd155a
officialezekiel <officialezekiel1@gmail.com>
2023-03-20 07:32:40 -0700

    printing to standaard output

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
-
+#include <unistd.h>
 /**
  * main - printing to standard error
  * Return: 1 (Success)
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
