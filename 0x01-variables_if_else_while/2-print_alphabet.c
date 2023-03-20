470cc8cec0cd81afc1fed1497c7dad38578f87b0
officialezekiel <officialezekiel1@gmail.com>
2023-03-20 15:38:35 -0700

    alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
- * main -Alphabet game entry point
- * Return: Always 0 (Success)
+ * main - Alphabet game entry point
+ * Return: 0 (Success)
  */
 
 int main(void)
