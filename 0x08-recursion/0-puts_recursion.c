9dac9f287592c42bc0d890be4e45ecc8ea8232d2
officialezekiel <officialezekiel1@gmail.com>
2023-04-06 19:32:53 -0700

    my first commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,14 +7,14 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\n')
-	{
-		_putchar('\n');
-		return;
-	}
-	else
+	if (*s != '\0')
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+		return;
 	}
 }
